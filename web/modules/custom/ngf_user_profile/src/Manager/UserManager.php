5c6e0003dd476f93d13e3829a975589853d36804
Nikolay Lobachev <nikolay.lobachev@gmail.com>
2018-04-10 09:27:15 +0200

    Remove services of user list items and followed, move everything into user manager. Switch to flag module

--- web/modules/custom/ngf_user_profile/src/Manager/UserManager.php
+++ web/modules/custom/ngf_user_profile/src/Manager/UserManager.php
@@ -4,8 +4,12 @@ namespace Drupal\ngf_user_profile\Manager;
 
 use Drupal\Core\Messenger\MessengerInterface;
 use Drupal\Core\Session\AccountInterface;
+use Drupal\user\Entity\User;
 use Drupal\user\UserDataInterface;
 use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
+use Drupal\flag\FlagService;
+use Drupal\ngf_user_profile\Helper\UserHelper;
+use Drupal\ngf_user_profile\Entity\UserList;
 
 class UserManager {
   /**
@@ -30,12 +34,11 @@ class UserManager {
   protected $userData;
 
   /**
-   * The messenger service.
+   * The flag service.
    *
-   * @var \Drupal\user\UserDataInterface
+   * @var \Drupal\flag\FlagService
    */
   protected $flag;
-
 
   /**
    * UserProfileController constructor.
@@ -46,11 +49,14 @@ class UserManager {
    *   The messenger service.
    * @param \Drupal\user\UserDataInterface $userData
    *   The user data service.
+   * @param \Drupal\flag\FlagService $flag
+   *   The flag service.
    */
-  public function __construct(AccountInterface $current_user, MessengerInterface $messenger, UserDataInterface $userData) {
+  public function __construct(AccountInterface $current_user, MessengerInterface $messenger, UserDataInterface $userData, FlagService $flag) {
     $this->currentUser = $current_user;
     $this->messenger = $messenger;
     $this->userData = $userData;
+    $this->flag = $flag;
 
     $this->checkAccess();
   }
@@ -67,6 +73,257 @@ class UserManager {
 
   protected function addMessage($message) {
     $this->messenger->addMessage($message, MessengerInterface::TYPE_STATUS);
+  }
+
+  public function getList($list_id) {
+    $list_ids = \Drupal::entityQuery('ngf_user_list')
+      ->condition('user_id', $this->currentUser->id())
+      ->condition('list_id', $list_id)
+      ->execute();
+
+    return UserList::loadMultiple($list_ids);
+  }
+
+  public function getFollowedList() {
+    $followed_user_items = $this->getUserFlaggedItemsByFlagId('ngf_follow_user');
+    $followed_user_ids = [];
+    foreach ($followed_user_items as $user_item) {
+      $followed_user_ids[] = $user_item->entity_id;
+    }
+    return User::loadMultiple($followed_user_ids);
+  }
+
+  public function getUserFlaggedItemsByFlagId($flag_id) {
+    // Use a query.
+    $query = \Drupal::database()
+      ->select('flagging', 'f')
+      ->fields('f', array())
+      ->condition('flag_id', $flag_id)
+      ->condition('uid', $this->currentUser->id());
+
+    return $query->execute()->fetchAll();
+  }
+
+  /**
+   * Follow user.
+   *
+   * @param string $username
+   *   Username.
+   */
+  public function follow($username) {
+    $user = user_load_by_name($username);
+    if (empty($user)) {
+      $this->messenger->addMessage(t('User is not found.'), MessengerInterface::TYPE_ERROR);
+    }
+    else {
+      $user_name = UserHelper::getUserFullName($user);
+      $flag_id = 'ngf_follow_user';
+      $flag = $this->flag->getFlagById($flag_id);
+      if (!empty($flag)) {
+        if (!$flag->isFlagged($user, $this->currentUser)) {
+          $this->flag->flag($flag, $user);
+
+          $this->messenger->addMessage(t('You are following @username',
+            ['@username' => $user_name]), MessengerInterface::TYPE_STATUS);
+        } else {
+          $this->messenger->addMessage(t('You are already following @username',
+            ['@username' => $user_name]), MessengerInterface::TYPE_ERROR);
+        }
+      }
+      else {
+        $this->messenger->addMessage(t('Flag @flag_id is not found',
+          ['@flag_id' => $flag_id]), MessengerInterface::TYPE_STATUS);
+      }
+    }
+  }
+
+  /**
+   * Remove passed user to the list of followed users.
+   *
+   * @param string $username
+   *   Username.
+   */
+  public function unfollow($username) {
+    $user = user_load_by_name($username);
+
+    if (empty($user)) {
+      $this->messenger->addMessage(t('User is not found.'), MessengerInterface::TYPE_ERROR);
+    }
+    else {
+      $user_name = UserHelper::getUserFullName($user);
+      $flag_id = 'ngf_follow_user';
+      $flag = $this->flag->getFlagById($flag_id);
+      if (!empty($flag)) {
+        if ($flag->isFlagged($user, $this->currentUser)) {
+          $this->flag->unflag($flag, $user);
+
+          $this->messenger->addMessage(t('You are not following anymore @username',
+            ['@username' => $user_name]), MessengerInterface::TYPE_STATUS);
+        }
+        else {
+          $this->messenger->addMessage(t('You are not following @username',
+            ['@username' => $user_name]), MessengerInterface::TYPE_ERROR);
+        }
+      }
+      else {
+        $this->messenger->addMessage(t('Flag @flag_id is not found',
+          ['@flag_id' => $flag_id]), MessengerInterface::TYPE_STATUS);
+      }
+    }
+  }
+
+  /**
+   * Adds user list.
+   *
+   * @param $name
+   *   List name.
+   */
+  public function addUserList($name) {
+    if (empty($name)) {
+      $this->addError(t('Name of the list is empty.'));
+    }
+    else {
+      $lists = \Drupal::entityQuery('ngf_user_list')
+        ->condition('user_id', $this->currentUser->id())
+        ->condition('name', $name)
+        ->execute();
+
+      if (empty($lists)) {
+        $user_list_item = UserList::create([
+          'user_id' => $this->currentUser->id(),
+          'name' => $name,
+        ]);
+        $user_list_item->save();
+
+        if ($user_list_item) {
+          $this->addMessage(t('@name has been added', ['@name' => $name]));
+        } else {
+          $this->addError(t('Something went wrong, impossible to add @name list', ['@name' => $name]));
+        }
+      }
+      else {
+        $this->addError(t('You already have @name list', ['@name' => $name]));
+      }
+    }
+  }
+
+  public function getUserLists() {
+    return UserList::loadMultiple(\Drupal::entityQuery('ngf_user_list')
+      ->condition('user_id', $this->currentUser->id())
+      ->execute());
+  }
+
+  public function addUserListItem($list_id, $username) {
+    $list = UserList::load($list_id);
+    $user = user_load_by_name($username);
+
+    if (empty($user)) {
+      $this->addError(t('User is not found.'));
+    } elseif (empty($list)) {
+      $this->addError(t('List is not found.'));
+    } elseif ($list->getOwnerId() !== $this->currentUser->id()) {
+      $this->addError(t('This list does not belong to you.'));
+    } else {
+      $flag_id = 'ngf_list_item';
+      $flag = $this->flag->getFlagById($flag_id);
+      if (!empty($flag)) {
+        $user_name = UserHelper::getUserFullName($user);
+        if (!$flag->isFlagged($user, $this->currentUser)) {
+          $this->flag->flag($flag, $list, $user);
+          $this->addMessage(t('User @username has been added to the list ', ['@username' => $user_name]));
+        }
+        else {
+          $this->addError(t('You have user @username in the list', ['@username' => $user_name]));
+        }
+      }
+      else {
+        $this->messenger->addMessage(t('Flag @flag_id is not found',
+          ['@flag_id' => $flag_id]), MessengerInterface::TYPE_STATUS);
+      }
+    }
+  }
+
+  public function removeUserListItem($list_id, $username) {
+    $list = UserList::load($list_id);
+    $user = user_load_by_name($username);
+
+    if (empty($user)) {
+      $this->addError(t('User is not found.'));
+    } elseif (empty($list)) {
+      $this->addError(t('List is not found.'));
+    } elseif ($list->getOwnerId() !== $this->currentUser->id()) {
+      $this->addError(t('This list does not belong to you.'));
+    } else {
+      $flag_id = 'ngf_list_item';
+      $flag = $this->flag->getFlagById($flag_id);
+      if (!empty($flag)) {
+        $user_name = UserHelper::getUserFullName($user);
+        if ($flag->isFlagged($list, $user)) {
+          $this->flag->unflag($flag, $list, $user);
+          $this->addMessage(t('User @username has been removed from the list ', ['@username' => $user_name]));
+        }
+        else {
+          $this->addError(t('You do not have user @username in the list', ['@username' => $user_name]));
+        }
+      }
+      else {
+        $this->messenger->addMessage(t('Flag @flag_id is not found',
+          ['@flag_id' => $flag_id]), MessengerInterface::TYPE_STATUS);
+      }
+    }
+  }
+
+  public function getUserListItems($user_list_id) {
+    $list = UserList::load($user_list_id);
+    $user_list_items = [];
+    if (empty($list)) {
+      $this->addError(t('List is not found.'));
+    } else {
+      $flag_id = 'ngf_list_item';
+      $flag = $this->flag->getFlagById($flag_id);
+      if (!empty($flag)) {
+        $flag_user_list_items = $this->flag->getEntityFlaggings($flag, $list);
+        $user_ids = [];
+        foreach ($flag_user_list_items as $flag_user_list_item) {
+          $user_ids[] = $flag_user_list_item->getOwnerId();
+        }
+        $user_list_items =  User::loadMultiple($user_ids);
+      } else {
+        $this->addError(t('Flag @flag is not found.', ['@flag' => $flag_id]));
+      }
+    }
+
+    return $user_list_items;
+  }
+
+  public function removeUserList($list_id) {
+    $user_list = UserList::load($list_id);
+    if (empty($user_list)) {
+      $this->addError(t('List is not found.'));
+    } elseif ($user_list->getOwnerId() !== $this->currentUser->id()) {
+      $this->addError(t('This list does not belong to you.'));
+    } else {
+      // Remove related list items.
+      $this->removeUserListItemByList($user_list);
+
+      // Remove list.
+      $storage_handler = \Drupal::entityTypeManager()->getStorage('ngf_user_list');
+      $entities = $storage_handler->loadMultiple([$list_id]);
+      if (!empty($entities)) {
+        $storage_handler->delete($entities);
+      }
+
+      $this->addMessage(t('List "@name" have been removed', ['@name' => $user_list->getName()]));
+    }
+  }
+
+  protected function removeUserListItemByList($user_list) {
+    $flag = $this->flag->getFlagById('ngf_list_item');
+    if (empty($flag)) {
+      $this->addError(t('List is not found.'));
+    } else {
+      $this->flag->unflagAllByEntity($user_list);
+    }
   }
 
 
