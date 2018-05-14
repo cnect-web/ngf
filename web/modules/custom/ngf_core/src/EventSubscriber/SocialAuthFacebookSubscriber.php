fdaf6fb7e7a8312a588e7178cea20bedf5a84a42
Horacio Lopes <HoracioLopes@gmail.com>
2018-05-14 11:41:31 +0200

    Add NGF Core module + view modes + group views (profile, discover page, group content)

---
+++ web/modules/custom/ngf_core/src/EventSubscriber/SocialAuthFacebookSubscriber.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace Drupal\ngf_core\EventSubscriber;
+
+use Drupal\social_api\Plugin\NetworkManager;
+use Drupal\social_auth\Event\SocialAuthUserEvent;
+use Drupal\social_auth\Event\SocialAuthEvents;
+use Drupal\social_auth_facebook\FacebookAuthManager;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+
+/**
+ * Reacts on Social Auth events.
+ */
+class SocialAuthFacebookSubscriber implements EventSubscriberInterface {
+
+  /**
+   * Used to retrieve the token from session.
+   *
+   * @var \Symfony\Component\HttpFoundation\Session\SessionInterface
+   */
+  protected $session;
+
+  /**
+   * Used to get an instance of the SDK used by the Social Auth implementer.
+   *
+   * @var \Drupal\social_api\Plugin\NetworkManager
+   */
+  protected $networkManager;
+
+  /**
+   * The Social Auth Facebook manager.
+   *
+   * @var \Drupal\social_auth_facebook\FacebookAuthManager
+   */
+  protected $facebookManager;
+
+  /**
+   * SocialAuthSubscriber constructor.
+   *
+   * @param \Symfony\Component\HttpFoundation\Session\SessionInterface $session
+   *   Used to retrieve the token from session.
+   * @param \Drupal\social_api\Plugin\NetworkManager $network_manager
+   *   Used to get an instance of the SDK used by the Social Auth implementer.
+   * @param \Drupal\social_auth_google\GoogleAuthManager $google_manager
+   *   The Social Auth Google manager.
+   */
+  public function __construct(SessionInterface $session, NetworkManager $network_manager, FacebookAuthManager $facebookManager) {
+    $this->session = $session;
+    $this->networkManager = $network_manager;
+    $this->facebookManager = $facebookManager;
+  }
+
+  /**
+   * {@inheritdoc}
+   *
+   * Returns an array of event names this subscriber wants to listen to.
+   * For this case, we are going to subscribe for user creation and login
+   * events and call the methods to react on these events.
+   */
+  public static function getSubscribedEvents() {
+    $events[SocialAuthEvents::USER_CREATED] = ['onUserCreated'];
+    $events[SocialAuthEvents::USER_LOGIN] = ['onUserLogin'];
+
+    return $events;
+  }
+
+  /**
+   * Alters the user name.
+   *
+   * @param \Drupal\social_auth\Event\SocialAuthUserEvent $event
+   *   The Social Auth user event object.
+   */
+  public function onUserCreated(SocialAuthUserEvent $event) {
+    if ($event->getPluginId() == 'social_auth_facebook') {
+      $fbProfile = $this->facebookManager->getUserInfo();
+      $user = $event->getUser();
+      $user->setUsername($fbProfile->getName());
+      $user->field_ngf_first_name->value = $fbProfile->getFirstName();
+      $user->field_ngf_last_name->value = $fbProfile->getLastName();
+      $user->field_ngf_biography->value = $fbProfile->getBio();
+      $user->save();
+    }
+  }
+
+  /**
+   * Sets a drupal message when a user logs in.
+   *
+   * @param \Drupal\social_auth\Event\SocialAuthUserEvent $event
+   *   The Social Auth user event object.
+   */
+  public function onUserLogin(SocialAuthUserEvent $event) {
+     // If user logs in using social_auth_google and the access token exists.
+    if ($event->getPluginId() == 'social_auth_facebook') {
+    }
+  }
+
+}
+
+
---
+++ web/modules/custom/ngf_core/src/Routing/NGFCoreRouteSubscriber.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace Drupal\ngf_core\Routing;
+
+use Drupal\group\Entity\GroupInterface;
+use Drupal\node\Entity\NodeType;
+use Drupal\Core\Routing\RoutingEvents;
+use Drupal\Core\Routing\RouteSubscriberBase;
+use Symfony\Component\Routing\RouteCollection;
+
+/**
+ * Listens to the dynamic route events.
+ */
+class NGFCoreRouteSubscriber extends RouteSubscriberBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function getSubscribedEvents() {
+    $events[RoutingEvents::ALTER] = ['onAlterRoutes', -9999];
+    return $events;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function alterRoutes(RouteCollection $collection) {
+    if ($route = $collection->get('user.login')) {
+      $route->setDefault("_title_callback", '\Drupal\ngf_core\Routing\NGFCoreRouteSubscriber::loginTitle');
+    }
+
+    if ($route = $collection->get('entity.group_content.create_form')) {
+      $route->setDefault("_title_callback", '\Drupal\ngf_core\Routing\NGFCoreRouteSubscriber::gcCreateFormTitle');
+    }
+
+    // Switch group paths controllers.
+    if ($route = $collection->get('entity.group.join')) {
+      $route->setDefault('_controller', '\Drupal\ngf_core\Controller\NGFGroupMembershipController::join');
+    }
+
+    // Unset membership request route.
+    if ($route = $collection->get('entity.group.request_membership')) {
+      $collection->remove('entity.group.request_membership');
+    }
+  }
+
+  /**
+   * Callback for the login page title.
+   *
+   * @Param GroupInterface $group     A Group entity.
+   * @Param string         $plugin_id The plugin id.
+   *
+   * @return string
+   *   A string to use as the title.
+   */
+  public function loginTitle() {
+    return t('Sign In');
+  }
+
+  /**
+   * Callback for the group content creation form title.
+   *
+   * @Param GroupInterface $group     A Group entity.
+   * @Param string         $plugin_id The plugin id.
+   *
+   * @return string
+   *   A string to use as the title.
+   */
+  public function gcCreateFormTitle(GroupInterface $group, $plugin_id) {
+    $plugin = $group->getGroupType()->getContentPlugin($plugin_id);
+    $content_type = NodeType::load($plugin->getEntityBundle());
+    $t_vars = ['@name' => $content_type->label(), '@group' => $group->label()];
+    return t('Add @name in @group', $t_vars);
+  }
+
+}
