fcee1298c1d3146d848185c4fcd70a696ccb5f1d
Nikolay Lobachev <nikolay.lobachev@gmail.com>
2018-06-22 18:15:47 +0200

    Add ngf_search module

---
+++ web/modules/custom/ngf_search/src/Manager/GroupAccessManager.php
@@ -0,0 +1,12 @@
+<?php
+namespace Drupal\ngf_search\Manager;
+
+use Drupal\node\NodeInterface;
+use Drupal\user\UserInterface;
+
+class GroupAccessManager {
+  public function checkNode(NodeInterface $node, UserInterface $user) {
+
+  }
+
+}
